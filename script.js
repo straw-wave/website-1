ab5784e5a587df27e8aee76820ed2bf56192f6b3
straw-wave <42402337+straw-wave@users.noreply.github.com>
2018-09-19 16:05:52 +0500

    Added the button and window.open method.

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-alert("Welcome to the Big Page!");
+//alert("Welcome to the Big Page!");
+
+function myFunction() {
+	alert("You have clicked!!!");
+}
+function openWindow() {
+	window.open("page_2.html", "_self")
+}
+
